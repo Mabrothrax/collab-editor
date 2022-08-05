f6e5d7c09b266531d69ff7394897ef3c70e4eb72
Adrian Mucha <therottengod@gmail.com>
2022-08-05 09:23:09 +0200

    fix: lint

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 const https = require('https');
-const url = require("url");
+const url = require('url');
 const fs = require('fs');
-const { Server } = require("socket.io");
-const dataBank = require("./db/databank.json");
+const { Server } = require('socket.io');
+const dataBank = require('./db/databank.json');
 const options = {
     key: fs.readFileSync('./cert/key.pem'),
     cert: fs.readFileSync('./cert/cert.pem')
 };
 
-if(process.env.NODE_ENV === "dev") {
+if(process.env.NODE_ENV === 'dev') {
     process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 }
 
 const server = https.createServer(options, (req, res) => {
     const reqPath = url.parse(req.url).pathname;
-    if (req.method == "GET") {
-        if (reqPath == "/") {
+    if (req.method == 'GET') {
+        if (reqPath == '/') {
             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
             fs.createReadStream('./views/collab_form.html').pipe(res);
         } else {
             res.writeHead(404, {'Content-Type': 'text/plain; charset=utf-8'});
-            res.write("Nothing to see here");
+            res.write('Nothing to see here');
             res.end();
         }
     }
